40f35edac51ec3a6f7cb87d18b786b28f521a863
kimsangyoon1004 <eric_yoon1@naver.com>
2025-06-19 00:42:13 +0900

    test API

---
+++ restAPI/practice/src/main/java/com/practice/practice/PracticeApplication.java
@@ -0,0 +1,24 @@
+package com.practice.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeApplication.class, args);
+	}
+
+}
+
+@RestController
+class AController {
+    @GetMapping("/practice1")
+    public response practice() {
+        return new response("this is to test api");
+    }
+}
+
---
+++ restAPI/practice/src/main/java/com/practice/practice/response.java
@@ -0,0 +1,13 @@
+package com.practice.practice;
+
+public class response {
+    private String message;
+
+    public response(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
