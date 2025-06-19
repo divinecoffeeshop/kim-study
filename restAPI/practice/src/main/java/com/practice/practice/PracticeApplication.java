c716ad0ccf76958ae0023964d01afdb18935565a
kimsangyoon1004 <eric_yoon1@naver.com>
2025-06-19 16:36:18 +0900

    restAPI practice

--- restAPI/practice/src/main/java/com/practice/practice/PracticeApplication.java
+++ restAPI/practice/src/main/java/com/practice/practice/PracticeApplication.java
@@ -18,7 +18,7 @@ public class PracticeApplication {
 class AController {
     @GetMapping("/practice1")
     public response practice() {
-        return new response("this is to test api practice web");
+        return new response("this is to test api practice restAPI");
     }
 }
 
